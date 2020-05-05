37bb851b12d3ac92177117059cc206bcff1b9d78
barbatof-crystaleye <>
2020-05-05 16:29:26 +0200

    Create DependInfo.cmake

---
+++ CADMesh/examples/basic/build/CMakeFiles/cadmesh_example.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/guarino/Esperimenti/gamma/CADMesh/examples/basic/cadmesh_example.cc" "/home/guarino/Esperimenti/gamma/CADMesh/examples/basic/build/CMakeFiles/cadmesh_example.dir/cadmesh_example.cc.o"
+  "/home/guarino/Esperimenti/gamma/CADMesh/examples/basic/src/DetectorConstruction.cc" "/home/guarino/Esperimenti/gamma/CADMesh/examples/basic/build/CMakeFiles/cadmesh_example.dir/src/DetectorConstruction.cc.o"
+  "/home/guarino/Esperimenti/gamma/CADMesh/examples/basic/src/PhysicsList.cc" "/home/guarino/Esperimenti/gamma/CADMesh/examples/basic/build/CMakeFiles/cadmesh_example.dir/src/PhysicsList.cc.o"
+  "/home/guarino/Esperimenti/gamma/CADMesh/examples/basic/src/PrimaryGeneratorAction.cc" "/home/guarino/Esperimenti/gamma/CADMesh/examples/basic/build/CMakeFiles/cadmesh_example.dir/src/PrimaryGeneratorAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4UI_USE"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/guarino/Esperimenti/gamma/geant4.10.04.p02/geant4.10.04-install/include/Geant4"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
