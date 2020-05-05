a09d8385dfaf355fb0a8ea8cc81f232452613fa8
barbatof-crystaleye <>
2020-05-05 16:29:20 +0200

    Create cmake_clean.cmake

---
+++ CADMesh/examples/basic/build/CMakeFiles/cadmesh_example.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cadmesh_example.dir/cadmesh_example.cc.o"
+  "CMakeFiles/cadmesh_example.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/cadmesh_example.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/cadmesh_example.dir/src/PrimaryGeneratorAction.cc.o"
+  "cadmesh_example.pdb"
+  "cadmesh_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cadmesh_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
